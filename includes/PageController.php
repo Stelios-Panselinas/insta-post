eaa305874331cbc991dcf1cff862c22b7b6864f4
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2023-09-02 00:13:40 +0300

    Update PageController.php

--- includes/PageController.php
+++ includes/PageController.php
@@ -3,8 +3,8 @@ class PageController {
     public function renderPage($template, $data = []) {
         extract($data);
 
-        include('../templates/header.php');
-        include('../templates/userHome.php');
-        include('../templates/footer.php');
+        include('templates/header.php');
+        include('templates/userHome.php');
+        include('templates/footer.php');
     }
 }
