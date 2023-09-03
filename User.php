633c0fd7164faff7d531984334395ee943b724ff
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2023-09-04 01:05:48 +0300

    update password function

--- User.php
+++ User.php
@@ -17,16 +17,35 @@ class User extends Database {
         $stmt->bind_param('si', $newPass, $user_id);
         $stmt->execute();
     }
+
+    public function validateOldPassword($oldPassword, $user_id){
+        $this->connect();
+
+        $stmt = $this->prepare('SELECT password FROM user WHERE user_id=?');
+        $stmt->bind_param('i', $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $pass = $row['password'];
+        if (strcmp($pass,$oldPassword) == 0){
+            echo 'false';
+        }else{
+            echo 'true';
+        }
+    }
 }
 
+$user = new User();
 if ($_GET['function'] == "updateUsername") {
     $user_id = 1; //$_SESSION
     $username = $_GET['username'];
-    $user = new User();
     $user->updateUsername($username, $user_id);
 }elseif ($_GET['function'] == 'updatePassword'){
     $user_id = 1;
     $password = $_GET['newPass'];
-    $user = new User();
     $user->updatePassword($user_id, $password);
+}elseif ($_GET['function'] == 'validateOldPassword'){
+    $user_id = 1;
+    $oldPassword = $_GET['oldPassword'];
+    $user->validateOldPassword($oldPassword,$user_id);
 }
--- userJS.js
+++ userJS.js
@@ -1,11 +1,54 @@
 function updateUsername(){
     let newUsername = document.getElementById('username').value;
     const url = 'User.php?function=updateUsername&username='+newUsername;
-    fetch(url);
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url);
+    xhttp.send();
 }
 
 function updatePassword(){
-    let newPass = document.getElementById('newpwd');
-    const url = 'User.php?function=updatePassword$newPass='+newPass;
-    fetch(url);
+        let newPass = document.getElementById('newpwd').value;
+        let oldPass = document.getElementById('oldpwd').value;
+        let rePass = document.getElementById('reppwd').value;
+
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function () {
+            console.log(this.responseText == 'true');
+            if(this.responseText == 'true'){
+                setText();
+            }
+        }
+        xhttp.open("GET", 'User.php?function=validateOldPassword&oldPassword='+oldPass);
+        xhttp.send();
+
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        if (!passwordRegex.test(newPass)) {
+            alert("New password must contain at least one uppercase letter, one number, and one special character, and be at least 8 characters long.");
+            return;
+        }
+
+        if (newPass !== rePass) {
+            document.getElementById('passwordMessage').innerText = "New password do not match with the new password!";
+            setTimeout(hideElement(), 1500);
+            return;
+        }
+
+        // Send the current and new passwords to the server via AJAX (not shown here)
+
+        // Clear the form and display a success message (replace with your logic)
+        document.getElementById("currentPassword").value = "";
+        document.getElementById("newPassword").value = "";
+        document.getElementById("confirmPassword").value = "";
+        document.getElementById("passwordMessage").textContent = "Password changed successfully.";
+
+}
+
+function hideElement() {
+        document.getElementById('oldPassMess').innerText = "";
+}
+
+function setText(){
+    document.getElementById('oldPassMess').innerText = 'Password is not correct!';
+    setTimeout(hideElement(),1500);
 }
