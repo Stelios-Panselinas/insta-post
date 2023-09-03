544265877e2515a9ba560db05d8e1459075b31e4
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2023-09-03 19:34:00 +0300

    Revert "φιχεσ ον θσερ.πηπ"
    This reverts commit 7bf463979052bef3bbcd71827134bf34d5e7359a.

--- User.php
+++ User.php
@@ -6,25 +6,13 @@ class User extends Database {
 
         $stmt = $this->prepare("UPDATE user SET first_name=? WHERE user_id=?");
         $stmt->bind_param("si", $username, $user_id);
-        if ($stmt->execute()) {
-            // Query executed successfully
-            echo "Username updated successfully";
-        } else {
-            // Query failed
-            echo "Error: " . $stmt->error;
-        }
-
+        $stmt->execute();
     }
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = json_decode(file_get_contents("php://input"), true);
-
-    if ($_GET['function'] == "updateUsername") {
-        $user_id = 1; // Replace with your logic to get the user ID (e.g., from session)
-        $username = $data['username'];
-
-        $user = new User();
-        $user->updateUsername($username, $user_id);
-    }
+if ($_GET['function'] == "updateUsername") {
+    $user_id = 1; //$_SESSION
+    $username = $_GET['username'];
+    $user = new User();
+    $user->updateUsername($username, $user_id);
 }
--- userJS.js
+++ userJS.js
@@ -1,34 +1,5 @@
-function updateUsername() {
-    // Get the new username from the input field
+function updateUsername(){
     let newUsername = document.getElementById('username').value;
-
-    // Create an object to specify the fetch request options
-    const requestOptions = {
-        method: 'POST',  // Use POST method to send data to the server
-        body: JSON.stringify({ username: newUsername }), // Convert data to JSON format
-        headers: {
-            'Content-Type': 'application/json' // Set the content type to JSON
-        }
-    };
-
-    // Define the URL for your PHP script
-    const url = 'User.php?function=updateUsername';
-
-    // Send the fetch request with the specified options
-    fetch(url, requestOptions)
-        .then(response => {
-            console.log(response);
-            // Check if the response status is OK (HTTP 200)
-            if (response.ok) {
-                // Handle a successful response here if needed
-                console.log('Username updated successfully');
-            } else {
-                // Handle errors or non-OK responses here
-                console.error('Failed to update username');
-            }
-        })
-        .catch(error => {
-            // Handle any network or fetch-related errors here
-            console.error('Error:', error);
-        });
+    const url = 'User.php?function=updateUsername&username='+newUsername;
+    fetch(url);
 }
