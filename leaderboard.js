aa7030580f1c2b199b68018a8d2375e6e150d952
Rafael-N-M <rafmichos@gmail.com>
2023-09-07 14:42:48 +0300

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,10 +1,12 @@
 
 function leadertable () {
 
+    document.getElementById("leaderboard");
+
     const xhttp = new XMLHttpRequest();
         xhttp.onload = function () {         
 }
-xhttp.open("GET", "leaderboard.php?q" );
+xhttp.open("GET", "leaderboard.php?" );
 xhttp.send();
 
 } 
