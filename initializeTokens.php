e7a43266c803a900c45ab139035561e5e1a33a26
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2023-10-03 00:46:47 +0300

    fix cronjob

--- initializeTokens.php
+++ initializeTokens.php
@@ -10,6 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$result = $conn->query("CALL tokens_distribution()");
 $result = $conn->query("UPDATE user SET user.total_tokens = total_tokens + cur_tokens;");
 $result = $conn->query("UPDATE user SET user.total_score = total_score + score;");
 $result = $conn->query("UPDATE user SET user.cur_tokens = 0;");
